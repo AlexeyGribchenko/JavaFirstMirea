5bd979c1c5a207f9dc4742d72ade057ecaa52fef
Alexey Gribchenko <gribchenko.2003@mail.ru>
2022-09-02 11:27:28 +0300

    test commit

--- src/ru/mirea/test/TestFromTeacher.java
+++ src/ru/mirea/test/TestFromTeacher.java
@@ -7,5 +7,6 @@ public class TestFromTeacher {
         System.out.println("Это класс для тестирования;");
         System.out.println("Если он запускается - скорее всего, у вас всё настроено правильно;");
         System.out.println("Вы можете удалить этот класс и пакет.");
+        System.out.println("Что-то пишем, а что, не понятно.");
     }
 }
