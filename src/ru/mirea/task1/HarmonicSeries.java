ac74ffa11dfb03bbe6af43d6eb279c83b77b37ac
Alexey Gribchenko <gribchenko.2003@mail.ru>
2022-09-23 09:48:26 +0300

    task 1

--- src/ru/mirea/task1/HarmonicSeries.java
+++ src/ru/mirea/task1/HarmonicSeries.java
@@ -3,8 +3,7 @@ package ru.mirea.task1;
 public class HarmonicSeries {
     public static void run() {
         for (float i = 1; i <= 10; i++) {
-            System.out.print(1 / i);
-            System.out.print(' ');
+            System.out.print(String.format("%.2f ", (1 / i)));
         }
     }
 }
--- src/ru/mirea/task1/Main.java
+++ src/ru/mirea/task1/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         if (args.length == 0) {
-            System.out.println("Enter \"1\" to run Task_1_1. Other arguments will be ignored.");
-            System.out.println("Enter \"2\" and any other arguments to run Task_1_2.");
-            System.out.println("Enter \"3\" to run Task_1_3. Other arguments will be ignored.");
-            System.out.println("Enter \"4\" to run Task_1_4. Other arguments will be ignored.");
-            System.out.println("Enter \"5\" and one positive number to run Task_1_5.");
+            System.out.println("Enter \"1\" to run AverageSum.");
+            System.out.println("Enter \"2\" to run SumMinMax.");
+            System.out.println("Enter \"3\" and any other arguments to run Arguments.");
+            System.out.println("Enter \"4\" to run HarmonicSeries.");
+            System.out.println("Enter \"5\" to run Factorial.");
         } else {
             switch (args[0]) {
                 case "1" -> SumAverage.run();
